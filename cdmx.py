b2a669356a107a57fa81841a2a6dc83f06ed6de1
YahirAranda07 <0286935@up.edu.mx>
2026-06-02 19:51:13 -0600

    Update cdmx.py

--- cdmx.py
+++ cdmx.py
@@ -6,7 +6,7 @@ import folium
 import requests
 import joblib
 from streamlit_folium import st_folium
-
+ 
 # ── Configuración de la página ──────────────────────────────
 st.set_page_config(
     page_title="Inversión Inmobiliaria CDMX",
